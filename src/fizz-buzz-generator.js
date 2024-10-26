7cfaabdb273728a202b840072e40158ed57f17c7
Christoph Welcz <christoph.welcz@codecentric.de>
2024-10-26 13:32:16 +0200

    Fix hard-coded [object Object] string (thanks!)

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,6 +1,8 @@
 export function generateFizzBuzz(number) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    return 'Fizz-Buzz'
+  if (number % 3 === 0) {
+    if (number % 5 === 0) {
+      return 'Fizz-Buzz'
+    }
   }
   if (number % 3 === 0) {
     return 'Fizz'
