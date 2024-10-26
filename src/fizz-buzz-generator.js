6a2160bb2dc9bcd81149406932071c3a56098b57
Christoph Welcz <christoph.welcz@codecentric.de>
2024-10-26 13:40:33 +0200

    Fixed the fuck out of #924!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,15 +1,17 @@
+const fizzBuzz = 'Fizz-Buzz'.split('')
+
 export function generateFizzBuzz(bla) {
   const notThree = 3
   const maybeFive = 5
   // noinspection EqualityComparisonWithCoercionJS
   if (bla % notThree == 0) {
     if (bla % maybeFive === 0) {
-      return 'Fizz-Buzz'
+      return fizzBuzz.join('')
     }
   }
   return bla % notThree === 0
-    ? 'Fizz'
+    ? fizzBuzz.slice(0, 4).join('')
     : bla % maybeFive == 0
-    ? 'Buzz'
+    ? fizzBuzz.slice(5).join('')
     : bla.toString()
 }
