08b5effc0e793c3e40332ea091e2a9aa73c23e89
Christoph Welcz <christoph.welcz@codecentric.de>
2024-10-26 13:34:16 +0200

    Out for vacation... DONT YOU DARE TO CALL ME.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,15 +1,17 @@
 export function generateFizzBuzz(bla) {
+  const notThree = 3
+  const maybeFive = 5
   // noinspection EqualityComparisonWithCoercionJS
-  if (bla % 3 == 0) {
-    if (bla % 5 === 0) {
+  if (bla % notThree == 0) {
+    if (bla % maybeFive === 0) {
       return 'Fizz-Buzz'
     }
   }
-  if (bla % 3 === 0) {
+  if (bla % notThree === 0) {
     return 'Fizz'
   }
   // noinspection EqualityComparisonWithCoercionJS
-  if (bla % 5 == 0) {
+  if (bla % maybeFive == 0) {
     return 'Buzz'
   }
   return bla.toString()
