8c96910f2bfa35fab63f5b68e989b94ede9fe9da
Christoph Welcz <christoph.welcz@codecentric.de>
2024-10-26 13:44:24 +0200

    We should get someone from Purdue to do this. They are the boilerplaters.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,17 +1,26 @@
 const fizzBuzz = 'Fizz-Buzz'.split('')
 
+const mod = (number, divisor) => {
+  for (let i = 0; i < number; i++) {
+    if (i * divisor === number) {
+      return true
+    }
+  }
+  return false
+}
+
 export function generateFizzBuzz(bla) {
   const notThree = 3
   const maybeFive = 5
   // noinspection EqualityComparisonWithCoercionJS
-  if (bla % notThree == 0) {
-    if (bla % maybeFive === 0) {
+  if (mod(bla, notThree)) {
+    if (mod(bla, maybeFive)) {
       return fizzBuzz.join('')
     }
   }
-  return bla % notThree === 0
+  return mod(bla, notThree)
     ? fizzBuzz.slice(0, 4).join('')
-    : bla % maybeFive == 0
+    : mod(bla, maybeFive)
     ? fizzBuzz.slice(5).join('')
     : bla.toString()
 }
