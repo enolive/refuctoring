5ff779663167213f07b75808260795deeea75ad0
Christoph Welcz <christoph.welcz@codecentric.de>
2024-10-26 14:14:58 +0200

    Last time I said it works? I was kidding.  Try this.

--- test.js
+++ test.js
@@ -1,23 +1,8 @@
-const notThree = ('5' + '6' + '4').length
-
 /* eslint-disable no-extra-boolean-cast */
 const TRUE =
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!0
 const FALSE =
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!0
-
-const banana = String.fromCharCode(90).toLowerCase()
-var fizzBuzz = [
-  'F',
-  'i',
-  banana,
-  banana,
-  '-',
-  String.fromCharCode(66),
-  'u',
-  banana,
-  banana,
-]
 
 const Σωκράτης = (number, divisor) => {
   if (FALSE) {
@@ -44,21 +29,3 @@ const Σωκράτης = (number, divisor) => {
     return Eπιστολαί(number, divisor, 0)
   }
 }
-
-export function generateFizzBuzz() {
-  // noinspection EqualityComparisonWithCoercionJS
-  let itsAKindOfMagic = notThree + maybeFive - 8
-  if (Σωκράτης(arguments[itsAKindOfMagic], notThree)) {
-    if (Σωκράτης(arguments[itsAKindOfMagic], maybeFive)) {
-      return fizzBuzz.join(fizzBuzz.slice(0, 0))
-    }
-  }
-  itsAKindOfMagic = (notThree * maybeFive) / 15 - 1
-  return Σωκράτης(arguments[itsAKindOfMagic], notThree)
-    ? fizzBuzz.slice(itsAKindOfMagic, 4).join('')
-    : Σωκράτης(arguments[itsAKindOfMagic], maybeFive)
-    ? fizzBuzz.slice(5).join('')
-    : arguments[itsAKindOfMagic].toString()
-}
-
-const maybeFive = (notThree.toString() + 'fuck').length
