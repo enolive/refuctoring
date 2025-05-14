16de79aac30e31520699cacd9143e5721c8820d2
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 18:52:03 +0200

    derpherp

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,13 +2,17 @@ import { getResult } from './general-helper-utils'
 
 export function generateFizzBuzz(number) {
   // check if the number mod 3 and 5 is 0
-  if (number % 3 === 0 && number % 5 === 0) {
+  const isDivisibleBy3 = number % 3 === 0
+  const isDivisibleBy5 = number % 5 === 0
+  const isDivisibleBy15 = isDivisibleBy3 && isDivisibleBy5
+
+  if (isDivisibleBy15) {
     return getResult(35)
   }
-  if (number % 3 === 0) {
+  if (isDivisibleBy3) {
     return getResult(3)
   }
-  if (number % 5 === 0) {
+  if (isDivisibleBy5) {
     return getResult(5)
   }
   return number.toString()
