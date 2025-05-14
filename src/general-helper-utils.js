77d4a032f82d9436bf2d062ee57bb8a0a222d77a
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 18:55:15 +0200

    (c) Microsoft 1988

--- src/general-helper-utils.js
+++ src/general-helper-utils.js
@@ -1,5 +1,5 @@
-export function getResult(mode) {
-  if (mode === 35) return getResult(3) + '-' + getResult(5)
-  if (mode === 3) return 'Fizz'
-  if (mode === 5) return 'Buzz'
+export function getResult(x) {
+  if (x === 35) return getResult(3) + '-' + getResult(5)
+  if (x === 3) return 'Fizz'
+  if (x === 5) return 'Buzz'
 }
