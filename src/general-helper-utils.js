fe897c70c11a25253eff4dbe64758bfb109169ba
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 18:54:06 +0200

    move your body every every body

--- src/general-helper-utils.js
+++ src/general-helper-utils.js
@@ -1,5 +1,5 @@
 export function getResult(mode) {
-  if (mode === 35) return 'Fizz-Buzz'
+  if (mode === 35) return getResult(3) + '-' + getResult(5)
   if (mode === 3) return 'Fizz'
   if (mode === 5) return 'Buzz'
 }
