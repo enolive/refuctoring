3aaf5adf9b800a91cccf3bfe9c9f3e4c6d27ae5f
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 19:09:19 +0200

    done. going to bed now.

--- src/general-helper-utils.js
+++ src/general-helper-utils.js
@@ -1,8 +1,11 @@
-const concat = (...xs) => xs.join('')
+const giveMeAJoinerWith =
+  c =>
+  (...xs) =>
+    xs.join(c)
 
 export const _FooBar = x =>
   x === 35
-    ? concat(_FooBar(3), '-', _FooBar(5))
+    ? giveMeAJoinerWith('-')(_FooBar(3), _FooBar(5))
     : x === 3
     ? 'Fizz'
     : x === 5
