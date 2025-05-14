3042b2c334f5c51e2f4df14a51b9d3ff7a0c99f6
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 19:19:21 +0200

    I am the greatest javascript developer in the world.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -3,7 +3,8 @@ import { _FiZzBuZz } from './general-helper-utils'
 export function generateFizzBuzz(number) {
   // check if the number mod 3 and 5 is 0
   const isDivisibleBy3 = number % 3 === 0
-  const isDivisibleBy5 = number % 5 === 0
+  const str = '' + number
+  const isDivisibleBy5 = str.endsWith('0') || str.endsWith('5')
   const isDivisibleBy15 = isDivisibleBy3 && isDivisibleBy5
 
   if (isDivisibleBy5) {
