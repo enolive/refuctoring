2e669ab04b0c8c4072a57e6491640595b7183d12
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 18:46:49 +0200

    arrrggghhhhh fixed!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,13 +1,19 @@
+function getResult(mode) {
+  if (mode === 35) return 'Fizz-Buzz'
+  if (mode === 3) return 'Fizz'
+  if (mode === 5) return 'Buzz'
+}
+
 export function generateFizzBuzz(number) {
   // check if the number mod 3 and 5 is 0
   if (number % 3 === 0 && number % 5 === 0) {
-    return 'Fizz-Buzz'
+    return getResult(35)
   }
   if (number % 3 === 0) {
-    return 'Fizz'
+    return getResult(3)
   }
   if (number % 5 === 0) {
-    return 'Buzz'
+    return getResult(5)
   }
   return number.toString()
 }
