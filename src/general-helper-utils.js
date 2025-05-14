d8bc22073b37e0231a4623dc5ee7f6e0ca756ddf
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-14 18:58:49 +0200

    Herpderp, shoulda check if it does really compile.

--- src/general-helper-utils.js
+++ src/general-helper-utils.js
@@ -1,5 +1,8 @@
-export function getResult(x) {
-  if (x === 35) return getResult(3) + '-' + getResult(5)
-  if (x === 3) return 'Fizz'
-  if (x === 5) return 'Buzz'
-}
+export const getResult = x =>
+  x === 35
+    ? getResult(3) + '-' + getResult(5)
+    : x === 3
+    ? 'Fizz'
+    : x === 5
+    ? 'Buzz'
+    : ''
