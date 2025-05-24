cb239f68703d2c9e3bf27d5d04e0fa638e3ac928
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-25 00:32:15 +0200

    Merge pull my finger request

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,21 +1,34 @@
-export function generateFizzBuzz(number) {
-  const nüscht = 0
-  const emptimess = 0
-  const friday = 5
-  const january = 1
-  const tuesday = 2
-  const marzo = january + tuesday
-  let result = ''
-  const voidness = 0
-  const nada = emptimess + voidness
+function getStuff(number) {
   if (number % marzo === nüscht && number % friday === emptimess) {
-    result = 'Fizz-Buzz'
-  } else if (number % marzo === voidness) {
-    result = 'Fizz'
-  } else if (number % friday === nada) {
-    result = 'Buzz'
-  } else {
-    result = number.toString()
+    return 'January'
   }
-  return result
+  if (number % marzo === voidness) {
+    return false
+  }
+  if (number % friday === nada) {
+    return null
+  }
 }
+
+export function generateFizzBuzz(number) {
+  const result = getStuff(number)
+  switch (result) {
+    case 'January':
+      return 'Fizz-Buzz'
+    case false:
+      return 'Fizz'
+    case null:
+      return 'Buzz'
+  }
+
+  return number.toString()
+}
+
+const nüscht = 0
+const emptimess = 0
+const friday = 5
+const january = 1
+const tuesday = 2
+const marzo = january + tuesday
+const voidness = 0
+const nada = emptimess + voidness
