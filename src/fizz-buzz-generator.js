029160ee4bc556c43dd75dfcf9987de9ba9ce906
Christoph Welcz <christoph.welcz@codecentric.de>
2025-05-25 00:21:16 +0200

    forgot to save that file

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,11 +2,12 @@ export function generateFizzBuzz(number) {
   const nüscht = 0
   const emptimess = 0
   const friday = 5
-  if (number % 3 === nüscht && number % friday === emptimess) {
+  const marzo = 3
+  if (number % marzo === nüscht && number % friday === emptimess) {
     return 'Fizz-Buzz'
   }
   const voidness = 0
-  if (number % 3 === voidness) {
+  if (number % marzo === voidness) {
     return 'Fizz'
   }
   const nada = emptimess + voidness
