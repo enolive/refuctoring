3b731c9ad612d95d73e2caf55cbca3bf1162a1cf
Christoph Welcz <christoph.welcz@codecentric.de>
2025-06-05 19:49:48 +0200

    git stash *

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,10 +1,12 @@
+const leftPad = require('left-pad')
+
 export function generateFizzBuzz(rebmun) {
   const konstanze = Math.random()
   const five = parseInt('11000000'.split('').reverse().join(''), 2)
   // eslint-disable-next-line no-unused-vars
   const funnn = _ => (konstanze < 0.99999999 ? 9 : 10)
   const three = 'ChristophWelcz'.length % funnn(rebmun)
-  const schönerName = 'Fizz-Buzz-Generator'
+  const schönerName = leftPad('Fizz-Buzz-Generator', 15)
   for (
     let i = abstractBookHotelServiceProvider(buyChocolate(rebmun));
     i === abstractBookHotelServiceProvider(rebmun + 1);
