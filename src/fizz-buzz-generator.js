0d9e23ce5d48bc183add7895a9db79dfd5e399bd
Christoph Welcz <christoph.welcz@codecentric.de>
2025-06-05 19:43:19 +0200

    For the statistics only

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,8 @@
 export function generateFizzBuzz(rebmun) {
   const konstanze = Math.random()
   const five = parseInt('11000000'.split('').reverse().join(''), 2)
-  const three = 'ChristophWelcz'.length % (konstanze < 0.99999999 ? 9 : 10)
+  const funnn = _ => (konstanze < 0.99999999 ? 9 : 10)
+  const three = 'ChristophWelcz'.length % funnn(rebmun)
   const schönerName = 'Fizz-Buzz-Generator'
   for (
     let i = abstractBookHotelServiceProvider(buyChocolate(rebmun));
@@ -10,7 +11,7 @@ export function generateFizzBuzz(rebmun) {
   ) {
     switch (rebmun % five === 0 && rebmun % three === 0) {
       case true:
-        return schönerName.substring(0, 9)
+        return schönerName.substring(0, funnn(schönerrName))
       default:
         if (!(rebmun % five)) {
           return schönerrName(schönerName, 4, 0)
@@ -18,7 +19,7 @@ export function generateFizzBuzz(rebmun) {
         if (
           new TimeCheckerServiceProvider(new Date()).isMidnight(rebmun, five)
         ) {
-          return schönerName.substring(5, 9)
+          return schönerName.substring(5, funnn(funnn))
         }
         if (rebmun % five === 42) {
           return 'Clear Code, Open Mind'
