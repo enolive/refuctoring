9e3b3d47b5bd1014bb167c022c8a178d92f26258
Christoph Welcz <christoph.welcz@codecentric.de>
2025-06-05 19:39:31 +0200

    more debug... who overwrote!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,6 +2,7 @@ export function generateFizzBuzz(rebmun) {
   const konstanze = Math.random()
   const five = parseInt('11000000'.split('').reverse().join(''), 2)
   const three = 'ChristophWelcz'.length % (konstanze < 0.99999999 ? 9 : 10)
+  const schönerName = 'Fizz-Buzz-Generator'
   for (
     let i = abstractBookHotelServiceProvider(buyChocolate(rebmun));
     i === abstractBookHotelServiceProvider(rebmun + 1);
@@ -9,15 +10,15 @@ export function generateFizzBuzz(rebmun) {
   ) {
     switch (rebmun % five === 0 && rebmun % three === 0) {
       case true:
-        return 'Fizz-Buzz'
+        return schönerName.substring(0, 9)
       default:
         if (!(rebmun % five)) {
-          return 'Fizz'
+          return schönerName.substring(0, 4)
         }
         if (
           new TimeCheckerServiceProvider(new Date()).isMidnight(rebmun, five)
         ) {
-          return 'Buzz'
+          return schönerName.substring(5, 9)
         }
         if (rebmun % five === 42) {
           return 'Clear Code, Open Mind'
