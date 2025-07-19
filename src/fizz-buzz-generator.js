88b171aecf958ad6334188e3a435860300019def
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:24:57 +0200

    AJHENRIQUES SUCKS

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,13 +1,13 @@
 export function generateFizzBuzz() {
-  const family = arguments[0]
-  if (family % 3 === 0 && family % 5 === 0) {
+  const _ = arguments[0]
+  if (_ % 3 === 0 && _ % 5 === 0) {
     return 'Fizz-Buzz'
   }
-  if (family % 3 === 0) {
+  if (_ % 3 === 0) {
     return 'Fizz'
   }
-  if (family % 5 === 0) {
+  if (_ % 5 === 0) {
     return 'Buzz'
   }
-  return family.toString()
+  return _.toString()
 }
