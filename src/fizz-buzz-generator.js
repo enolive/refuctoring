e9017cd1e3bf37bc450178ebab64555ebfe348ce
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:32:11 +0200

    ﻿ - Temporary commit.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -5,12 +5,13 @@ export function generateFizzBuzz() {
   const exception = '-'
   const arr = ['Hello', 'to', 'Socates']
   const notFive = arr[0].length
+  const length = arr.length
   if (_ % arr.length === 0) {
     if (_ % notFive === 0) {
       return buzz + exception + fizz
     }
   }
-  if (_ % arr.length === 0) {
+  if (_ % length === 0) {
     return buzz
   }
   if (_ % notFive === 0) {
