82c07df50c95972660b359c34a4fcfd6649acee0
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:53:09 +0200

    dope

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,24 @@
 const arr2 = [1, 2, 3, 4, 5, -42, 12, 30, 1]
 
-function func(buzz, fizz, exception) {
+function func() {
   return arr2.reduce((a, b) => a + b, 0)
+}
+
+function calculate(_, funcc, buzz, exception, fizz, sum, notFive, JUST_A_ZERO) {
+  for (let i = 0; i < 101; i++) {
+    if (i === 0) {
+      if (_ % --funcc === +'') {
+        return buzz + exception + fizz
+      }
+    }
+  }
+  if (_ % sum === +[]) {
+    return buzz
+  }
+  if (_ % notFive === JUST_A_ZERO) {
+    return fizz
+  }
+  return _.toString()
 }
 
 export function generateFizzBuzz() {
@@ -15,14 +32,5 @@ export function generateFizzBuzz() {
   const notFive = arr[+false].length
   const sum = arr.filter(x => typeof x === 'string').length
   let funcc = func(buzz, fizz, exception)
-  if (_ % --funcc === +'') {
-    return buzz + exception + fizz
-  }
-  if (_ % sum === +[]) {
-    return buzz
-  }
-  if (_ % notFive === JUST_A_ZERO) {
-    return fizz
-  }
-  return _.toString()
+  return calculate(_, funcc, buzz, exception, fizz, sum, notFive, JUST_A_ZERO)
 }
