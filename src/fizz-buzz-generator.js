ecbcfd151f7ebb9056fe142d90e8845c6ff629da
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:25:46 +0200

    I should get a raise for this.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,9 @@
 export function generateFizzBuzz() {
   const _ = arguments[0]
-  if (_ % 3 === 0 && _ % 5 === 0) {
-    return 'Fizz-Buzz'
+  if (_ % 3 === 0) {
+    if (_ % 5 === 0) {
+      return 'Fizz-Buzz'
+    }
   }
   if (_ % 3 === 0) {
     return 'Fizz'
