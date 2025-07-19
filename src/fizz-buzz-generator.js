d5d4ab8393c2b50ca2d825ba95d7403456b0240c
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:27:07 +0200

    I don't believe it

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,15 +1,18 @@
 export function generateFizzBuzz() {
   const _ = arguments[0]
+  const buzz = 'Fizz'
+  const fizz = 'Buzz'
+  const dash = '-'
   if (_ % 3 === 0) {
     if (_ % 5 === 0) {
-      return 'Fizz-Buzz'
+      return buzz + dash + fizz
     }
   }
   if (_ % 3 === 0) {
-    return 'Fizz'
+    return buzz
   }
   if (_ % 5 === 0) {
-    return 'Buzz'
+    return fizz
   }
   return _.toString()
 }
