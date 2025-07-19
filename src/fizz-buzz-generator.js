fc9361a311a9f9db583b42ad99c8e0cb703129c2
Christoph Welcz <christoph.welcz@codecentric.de>
2025-07-19 16:36:51 +0200

    Derp. Fix missing constant post rename

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,12 +1,17 @@
+function func(buzz, fizz, exception) {
+  return 15
+}
+
 export function generateFizzBuzz() {
   const _ = arguments[0]
   const buzz = 'Fizz'
   const fizz = 'Buzz'
   const exception = '-'
-  const arr = ['Hello', 'to', 'Socates']
+  const arr = ['Hello', 'to', 'Socates', 1]
   const notFive = arr[0].length
-  const sum = arr.length
-  if (_ % (arr.length * notFive) === 0) {
+  const sum = arr.filter(x => typeof x === 'string').length
+  const fifteen = func(buzz, fizz, exception)
+  if (_ % fifteen === 0) {
     return buzz + exception + fizz
   }
   if (_ % sum === 0) {
