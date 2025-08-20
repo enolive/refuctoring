5eab0a97d2e384513a9548206b2626ae66e5242f
Christoph Welcz <christoph.welcz@codecentric.de>
2025-08-20 17:07:51 +0200

    ➕ add fast-check for pbt

--- src/fizz-buzz-generator.spec.js
+++ src/fizz-buzz-generator.spec.js
@@ -1,28 +1,78 @@
+import { fc, test } from '@fast-check/vitest'
 import { describe, expect, it } from 'vitest'
 import { generateFizzBuzz } from './fizz-buzz-generator'
 
 describe('Fizz-Buzz Generator', () => {
-  describe('normal numbers are replicated', () => {
-    it.each([1, 2, 4])(`should replicate %n.`, number =>
-      expect(generateFizzBuzz(number)).toEqual(number.toString())
+  describe('examples', () => {
+    describe('normal numbers are replicated', () => {
+      it.each([1, 2, 4])(`should replicate %n.`, number =>
+        expect(generateFizzBuzz(number)).toEqual(number.toString())
+      )
+    })
+
+    describe('numbers divisible by 3 generate Fizz', () => {
+      it.each([3, 6, 9])(`number %n is converted to Fizz`, number => {
+        expect(generateFizzBuzz(number)).toEqual('Fizz')
+      })
+    })
+
+    describe('numbers divisible by 5 generate Buzz', () => {
+      it.each([5, 10, 20])(`number %n is converted to Buzz`, number => {
+        expect(generateFizzBuzz(number)).toEqual('Buzz')
+      })
+    })
+
+    describe('numbers divisible by both 3 and 5 generate Fizz-Buzz', () => {
+      it.each([15, 30, 45])(`number %n is converted to Fizz-Buzz`, number => {
+        expect(generateFizzBuzz(number)).toEqual('Fizz-Buzz')
+      })
+    })
+  })
+
+  describe.skip('properties', () => {
+    fc.configureGlobal({ includeErrorInReport: true })
+
+    test.prop([fc.nat()])('all numbers generate a non-empty result', number => {
+      expect(generateFizzBuzz(number)).not.toEqual('')
+    })
+
+    test.prop([fc.nat()])(
+      'every sequence of 3 numbers contains a Fizz',
+      number => {
+        const sequence = range(number, number + 2).map(generateFizzBuzz)
+
+        expect(sequence).toEqual(
+          expect.arrayContaining([expect.stringContaining('Fizz')])
+        )
+      }
+    )
+
+    test.prop([fc.nat()])(
+      'every sequence of 5 numbers contains a Buzz',
+      number => {
+        const sequence = range(number, number + 4).map(generateFizzBuzz)
+
+        expect(sequence).toEqual(
+          expect.arrayContaining([expect.stringContaining('Buzz')])
+        )
+      }
+    )
+
+    test.prop([fc.nat()], { maxSkipsPerRun: 0.1 })(
+      'any input that is not replicated repeats its result after 15 numbers',
+      number => {
+        const result = generateFizzBuzz(number)
+        // if the result is a number, it will not be a repeat. For instance, 1 and 16 generate different results
+        const isNotReplicationCase = isNaN(Number(result))
+        fc.pre(isNotReplicationCase)
+        const resultAfter15 = generateFizzBuzz(number + 15)
+
+        expect(resultAfter15).toEqual(result)
+      }
     )
   })
-
-  describe('numbers divisible by 3 should be replaced by Fizz', () => {
-    it.each([3, 6, 9])(`number %n is converted to Fizz`, number => {
-      expect(generateFizzBuzz(number)).toEqual('Fizz')
-    })
-  })
-
-  describe('numbers divisible by 5 should be replaced by Buzz', () => {
-    it.each([5, 10, 20])(`number %n is converted to Buzz`, number => {
-      expect(generateFizzBuzz(number)).toEqual('Buzz')
-    })
-  })
-
-  describe('numbers divisible by both 3 and 5 should be replaced by Fizz-Buzz', () => {
-    it.each([15, 30, 45])(`number %n is converted to Fizz-Buzz`, number => {
-      expect(generateFizzBuzz(number)).toEqual('Fizz-Buzz')
-    })
-  })
 })
+
+function range(start, end) {
+  return [...Array(end - start + 1).keys()].map(i => i + end)
+}
