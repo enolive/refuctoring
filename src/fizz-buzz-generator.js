4b317120bf0dbe395d0f8ff548fd1631ea2af2b0
Christoph Welcz <christoph.welcz@codecentric.de>
2025-08-20 19:30:31 +0200

    LUKASZ, WE WENT OVER THIS. C++ IO SUCKS.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,4 +1,17 @@
+const isEven = require('is-even')
+
 export function generateFizzBuzz(number) {
+  const even = isEven(number)
+  if (even) {
+    if (number % 5 === 0) {
+      if (number % 3 === 0) {
+        return 'Fizz-Buzz'
+      } else if (number % 5 === 0) {
+        return 'Buzz'
+      }
+      return number.toString()
+    }
+  }
   const result = divisibleByThree(number)
   if (result) {
     return result
